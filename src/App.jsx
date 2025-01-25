620f4cc105e2940a723765d4474acc89008cc77a
cloud <lilpark1024@gmail.com>
2025-01-25 21:05:56 +0900

    Day1

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+let post = '홍대 카페';
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{color: 'grey', fontSize :'16px'}}>블로그임</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h4>{post}</h4>
+    </div>
+  );
 }
 
 export default App
