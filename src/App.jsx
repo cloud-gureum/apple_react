4bf0b5d46a64f0d43847894b6a707b29baf8151c
cloud <lilpark1024@gmail.com>
2025-02-11 00:55:56 +0900

    Day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
+/*eslint-disable*/
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-let post = '홍대 카페';
+  let logo = 'ReactBlog';
+  let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [thumbs, changeThumbs] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color: 'grey', fontSize :'16px'}}>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <button onClick={() => {
+          let copy1 = [...title];
+          copy1.sort();
+          changeTitle(copy1);
+        }
+        }>가나다순정렬</button>
+      <button onClick={() => {
+          let copy2 = [...title];
+          copy2[0] = '여자 코트 추천';
+          changeTitle(copy2);
+        }
+        }>글수정</button>
+      <div className="list">
+        <h4>{title[0]} <span onClick={() => { changeThumbs(thumbs + 1) }}>👍</span> {thumbs} </h4>
+        <p>2월 11일 발행</p>
+      </div>
+
+
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 11일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 11일 발행</p>
+      </div>
     </div>
   );
 }
